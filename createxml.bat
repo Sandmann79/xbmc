7c8cf3f6b6e6b07072053ca015ddf27a3b75054d
Sandmann79 <sandmann79@gmx.net>
2014-09-24 15:39:38 +0200

    Update

--- createxml.bat
+++ createxml.bat
@@ -2,12 +2,12 @@
 setlocal enabledelayedexpansion
 
 echo ^<?xml version="1.0" encoding="UTF-8" standalone="yes"?^> > %~dp0addon.xml
+echo ^<addons^> >> %~dp0addon.xml
 for /f %%f in ('dir /b /a:d') do (
     if exist %%f\addon.xml for /f "delims=" %%a in (%%f\addon.xml) do (
         set line=%%a
         if not "!line:~0,5!"=="<?xml" echo %%a >> %~dp0addon.xml
     )
-    echo. >> %~dp0addon.xml
 )
 for /f "delims= " %%a in ('D:\Tools\fciv -md5 %~dp0addon.xml') do echo %%a > %~dp0addon.xml.md5
-pause
+echo ^</addons^> >> %~dp0addon.xml

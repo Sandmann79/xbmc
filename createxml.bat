3a20aa6dcd48897297553eacb72b48e7d907b167
Sandmann79 <sandmann79@gmx.net>
2014-09-24 16:29:39 +0200

    zip Archives

--- createxml.bat
+++ createxml.bat
@@ -1,13 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
+set tools_dir=%~dp0tools
+set zip_dir=%~dp0zip
 
 echo ^<?xml version="1.0" encoding="UTF-8" standalone="yes"?^> > %~dp0addon.xml
 echo ^<addons^> >> %~dp0addon.xml
-for /f %%f in ('dir /b /a:d') do (
-    if exist %%f\addon.xml for /f "delims=" %%a in (%%f\addon.xml) do (
+for /f %%f in ('dir /b /a:d') do if exist %%f\addon.xml (
+    for /f "delims=" %%a in (%%f\addon.xml) do (
         set line=%%a
         if not "!line:~0,5!"=="<?xml" echo %%a >> %~dp0addon.xml
     )
+    %tools_dir%\7z a %zip_dir%\%%f.zip %%f -tzip
 )
-for /f "delims= " %%a in ('D:\Tools\fciv -md5 %~dp0addon.xml') do echo %%a > %~dp0addon.xml.md5
+
+for /f "delims= " %%a in ('%tools_dir%\fciv -md5 %~dp0addon.xml') do echo %%a > %~dp0addon.xml.md5
 echo ^</addons^> >> %~dp0addon.xml
+pause

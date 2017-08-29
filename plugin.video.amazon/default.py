452797d43c148ccb4c978d71bfc7279c586f8a80
Sandmann79 <sandmann79@gmx.net>
2017-08-29 19:21:52 +0200

    db update for all items in main menu

--- plugin.video.amazon/default.py
+++ plugin.video.amazon/default.py
@@ -11,25 +11,27 @@ def modes():
     if not sys.argv[2]:
         Log('Version: %s' % addon.getAddonInfo('version'))
         Log('Unicode support: %s' % os.path.supports_unicode_filenames)
+        cm = {}
 
-        cm_watchlist = [(getString(30185) % 'Watchlist',
-                         'RunPlugin(%s?mode=appfeed&sitemode=ExportList&url=%s)' % (sys.argv[0], wl))]
-        cm_library = [(getString(30185) % getString(30060), 'RunPlugin(%s?mode=appfeed&sitemode=ExportList&url=%s)' % (sys.argv[0], lib)),
-                      (getString(30116), 'RunPlugin(%s?mode=appfeed&sitemode=RefreshList&url=%s)' % (sys.argv[0], lib))]
+        updateall = (getString(30103), 'RunPlugin(%s?mode=appfeed&sitemode=updateAll)' % sys.argv[0])
+        cm['wl'] = [(getString(30185) % 'Watchlist',
+                         'RunPlugin(%s?mode=appfeed&sitemode=ExportList&url=%s)' % (sys.argv[0], wl)), updateall]
+        cm['lib'] = [(getString(30185) % getString(30060), 'RunPlugin(%s?mode=appfeed&sitemode=ExportList&url=%s)' % (sys.argv[0], lib)),
+                      (getString(30116), 'RunPlugin(%s?mode=appfeed&sitemode=RefreshList&url=%s)' % (sys.argv[0], lib)), updateall]
 
-        updatemovie = [(getString(30103), 'RunPlugin(%s?mode=appfeed&sitemode=updateAll)' % sys.argv[0]),
-                       (getString(30102), 'RunPlugin(%s?mode=movies&sitemode=addMoviesdb&url=f)' % sys.argv[0])]
-        updatetv = [(getString(30103), 'RunPlugin(%s?mode=appfeed&sitemode=updateAll)' % sys.argv[0]),
-                    (getString(30105), 'RunPlugin(%s?mode=tv&sitemode=addTVdb&url=f)' % sys.argv[0])]
+        cm['movie'] = [updateall, (getString(30102), 'RunPlugin(%s?mode=movies&sitemode=addMoviesdb&url=f)' % sys.argv[0])]
+        cm['tv'] = [updateall, (getString(30105), 'RunPlugin(%s?mode=tv&sitemode=addTVdb&url=f)' % sys.argv[0])]
+        cm['search'] = [updateall]
 
         if updateRunning():
-            updatemovie = updatetv = [(getString(30135), 'RunPlugin(%s?mode=appfeed&sitemode=updateAll)' % sys.argv[0])]
+            updaterun = json.dumps((getString(30135), 'RunPlugin(%s?mode=appfeed&sitemode=updateAll)' % sys.argv[0]))
+            cm = json.loads(json.dumps(cm).replace(json.dumps(updateall), updaterun))
 
-        addDir('Watchlist', 'appfeed', 'ListMenu', wl, cm=cm_watchlist)
-        addDir(getString(30104), 'listmovie', 'LIST_MOVIE_ROOT', cm=updatemovie)
-        addDir(getString(30107), 'listtv', 'LIST_TV_ROOT', cm=updatetv)
-        addDir(getString(30108), 'appfeed', 'SEARCH_DB')
-        addDir(getString(30060), 'appfeed', 'ListMenu', lib, cm=cm_library)
+        addDir('Watchlist', 'appfeed', 'ListMenu', wl, cm=cm['wl'])
+        addDir(getString(30104), 'listmovie', 'LIST_MOVIE_ROOT', cm=cm['movie'])
+        addDir(getString(30107), 'listtv', 'LIST_TV_ROOT', cm=cm['tv'])
+        addDir(getString(30108), 'appfeed', 'SEARCH_DB', cm=cm['search'])
+        addDir(getString(30060), 'appfeed', 'ListMenu', lib, cm=cm['lib'])
 
         xbmcplugin.endOfDirectory(pluginhandle)
     else:

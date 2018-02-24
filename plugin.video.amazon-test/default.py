Sandmann79/xbmc

commit 90a7610359ef2c99c8d5cdebf66cf8989df7d776

prevent global access blocking

plugin.video.amazon-test/default.py
@@ -2097,7 +2097,7 @@ def getUrldata(mode, asin, retformat='json', devicetypeid='AOAGZA014O5RE', versi
     if extra:
         url += '&resourceUsage=ImmediateConsumption&consumptionType=Streaming&deviceDrmOverride=CENC' \
                '&deviceStreamingTechnologyOverride=DASH&deviceProtocolOverride=Https' \
-               '&deviceBitrateAdaptationsOverride=CVBR%2CCBR&audioTrackId=all&gascEnabled=true'
+               '&deviceBitrateAdaptationsOverride=CVBR%2CCBR&audioTrackId=all'
         url += '&videoMaterialType=' + vMT
         url += '&desiredResources=' + dRes
         url += '&supportedDRMKeyScheme=DUAL_KEY' if platform != osAndroid and 'PlaybackUrls' in dRes else ''

commit 944fc990a120b91baeca1c43663220905e329c6a

Fixed regression in 90a7610359ef2c99c8d5cdebf66cf8989df7d776 `gascEnabled=true` is mandatory on primevideo.com

plugin.video.amazon-test/default.py
@@ -2098,6 +2098,8 @@ def getUrldata(mode, asin, retformat='json', devicetypeid='AOAGZA014O5RE', versi
         url += '&resourceUsage=ImmediateConsumption&consumptionType=Streaming&deviceDrmOverride=CENC' \
                '&deviceStreamingTechnologyOverride=DASH&deviceProtocolOverride=Https' \
                '&deviceBitrateAdaptationsOverride=CVBR%2CCBR&audioTrackId=all'
+        if UsePrimeVideo:
+            url += '&gascEnabled=true'
         url += '&videoMaterialType=' + vMT
         url += '&desiredResources=' + dRes
         url += '&supportedDRMKeyScheme=DUAL_KEY' if platform != osAndroid and 'PlaybackUrls' in dRes else ''
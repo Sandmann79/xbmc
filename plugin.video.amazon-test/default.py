cbec386b943ded57ef35be0da6eb28153be0a1c3
Sandmann79 <sandmann79@gmx.net>
2018-06-27 20:43:19 +0200

    first attempt to resume exported content

--- plugin.video.amazon-test/default.py
+++ plugin.video.amazon-test/default.py
@@ -1912,11 +1912,12 @@ def IStreamPlayback(asin, name, trailer, isAdult, extern):
     player.resolve(listitem)
     starttime = time.time()
 
-    while not xbmc.abortRequested and player.isPlayingVideo():
-        player.video_lastpos = player.getTime()
-        if time.time() > starttime + player.interval:
-            starttime = time.time()
-            player.updateStream('PLAY')
+    while not xbmc.abortRequested and player.running:
+        if player.isPlayingVideo():
+            player.video_lastpos = player.getTime()
+            if time.time() > starttime + player.interval:
+                starttime = time.time()
+                player.updateStream('PLAY')
         sleep(1)
 
     del player
@@ -3259,28 +3260,55 @@ class AmazonPlayer(xbmc.Player):
         self.video_lastpos = 0
         self.video_totaltime = 0
         self.dbid = 0
+        self.seek = 0
         self.url = ''
         self.extern = ''
         self.asin = ''
         self.cookie = None
         self.interval = 180
+        self.running = False
 
     def resolve(self, li):
+        if not self.checkResume():
+            xbmcplugin.setResolvedUrl(pluginhandle, True, xbmcgui.ListItem())
+            xbmc.executebuiltin('Container.Refresh')
+            return
+        self.running = True
         xbmcplugin.setResolvedUrl(pluginhandle, True, li)
-        self.dbid = int('0' + getListItem('DBID'))
-        Log(self.dbid)
         self.PlayerInfo('Starting Playback')
+        if self.seek:
+            self.seekTime(self.seek)
+            self.PlayerInfo('Resuming Playback')
+
         self.updateStream('START')
-        Log('Video ContentType Movie? %s' % xbmc.getCondVisibility('VideoPlayer.Content(movies)'), 0)
-        Log('Video ContentType Episode? %s' % xbmc.getCondVisibility('VideoPlayer.Content(episodes)'), 0)
+        Log('Video ContentType Movie? %s' % xbmc.getCondVisibility('VideoPlayer.Content(movies)'), xbmc.LOGDEBUG)
+        Log('Video ContentType Episode? %s' % xbmc.getCondVisibility('VideoPlayer.Content(episodes)'), xbmc.LOGDEBUG)
+
+    def checkResume(self):
+        self.dbid = int('0' + getListItem('DBID'))
+        Log(self.dbid, xbmc.LOGDEBUG)
+        if not self.dbid:
+            return True
+        dbtype = getListItem('DBTYPE')
+        result = jsonRPC('VideoLibrary.Get%sDetails' % dbtype, 'resume,playcount', {'%sid' % dbtype: self.dbid})
+        position = int(result['episodedetails']['resume']['position'])
+        playcount = int(result['episodedetails']['playcount'])
+        Log(result, xbmc.LOGDEBUG)
+
+        if playcount:
+            return True
+        if position > 180:
+            sel = Dialog.contextmenu([getString(12022).format(time.strftime("%H:%M:%S", time.gmtime(position))), getString(12021)])
+            if sel > -1:
+                self.seek = position if sel == 0 else 0
+            else:
+                return False
+        return True
 
     def onPlayBackEnded(self):
         self.finished()
 
     def onPlayBackStopped(self):
-        self.finished()
-
-    def onPlayBackError(self):
         self.finished()
 
     def updateStream(self, event):
@@ -3290,36 +3318,34 @@ class AmazonPlayer(xbmc.Player):
 
     def finished(self):
         self.updateStream('STOP')
-        """
         if self.extern:
-            playcount = 1 if ((self.video_lastpos * 100) / self.video_totaltime) >= 90 else 0
+            playcount = 1 if (self.video_lastpos * 100) / self.video_totaltime >= 90 else 0
             watched = getListItem('PlayCount')
-
             if self.dbid:
                 dbtype = getListItem('DBTYPE')
-                Log(jsonRPC('VideoLibrary.Get%sDetails' % dbtype, 'resume,playcount', {'%sid' % dbtype: self.dbid}))
                 params = {'%sid' % dbtype: self.dbid,
                           'resume': {'position': 0 if playcount else self.video_lastpos,
                                      'total': self.video_totaltime},
                           'playcount': playcount}
-                res = ''if jsonRPC('VideoLibrary.Set%sDetails' % dbtype, '', params) is 'OK' else 'NOT '
+                res = '' if 'OK' in jsonRPC('VideoLibrary.Set%sDetails' % dbtype, '', params) else 'NOT '
                 Log('%sUpdated %sid(%s) with: pos(%s) total(%s) playcount(%s)' % (res, dbtype, self.dbid, self.video_lastpos, self.video_totaltime, playcount))
-                xbmc.executebuiltin('Container.Refresh')
             else:
                 Log('No DBID returned')
                 if playcount and not watched:
                     xbmc.executebuiltin("Action(ToggleWatched)")
-        """
+
+        xbmc.executebuiltin('Container.Refresh')
+        self.running = False
 
     def PlayerInfo(self, msg):
         while not self.isPlayingVideo():
             sleep(self.sleeptm)
-        while self.isPlayingVideo() and (self.getTime() >= self.getTotalTime()):
+        while self.isPlayingVideo() and (0 > self.getTime() >= self.getTotalTime()):
             sleep(self.sleeptm)
         if self.isPlayingVideo():
-            Log('%s: %s/%s' % (msg, self.getTime(), self.getTotalTime()))
             self.video_totaltime = self.getTotalTime()
             self.video_lastpos = self.getTime()
+            Log('%s: %s/%s' % (msg, self.video_lastpos, self.video_totaltime))
 
 
 args = dict(urlparse.parse_qsl(urlparse.urlparse(sys.argv[2]).query))

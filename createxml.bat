aaa377b09bd90faa81f5313da7aea79cc0d81e02
Sandmann79 <sandmann79@gmx.net>
2020-03-25 13:54:10 +0100

    add Kodi 19/Matrix repo

--- createxml.bat
+++ createxml.bat
@@ -1,16 +1,21 @@
 @echo off
 setlocal enabledelayedexpansion
 set tools_dir=%~dp0tools
-set arc_dir=%~dp0packages
-set repo=repository.sandmann79.plugins
+set arc_dir_py2=%~dp0packages
+set arc_dir_py3=%~dp0packages-py3
+set xml_dir_py3=%~dp0xml-py3
+set repo_py3=repository.sandmann79-py3.plugins
+set v_py3=3.0.0
+
 
 if not "%1"=="clean" (
-    if not exist %arc_dir% md %arc_dir%
-    echo ^<?xml version="1.0" encoding="UTF-8" standalone="yes"?^>> %arc_dir%\addons.xml
-    echo ^<addons^>>> %arc_dir%\addons.xml
+    if not exist %arc_dir_py2% md %arc_dir_py2%
+    echo ^<?xml version="1.0" encoding="UTF-8" standalone="yes"?^>> %arc_dir_py2%\addons.xml
+    echo ^<addons^>>> %arc_dir_py2%\addons.xml
+    if not exist %arc_dir_py3% md %arc_dir_py3%
+    echo ^<?xml version="1.0" encoding="UTF-8" standalone="yes"?^>> %arc_dir_py3%\addons.xml
+    echo ^<addons^>>> %arc_dir_py3%\addons.xml
 )
-
-if exist plugin.video.amazon\resources\cache rd /s /q plugin.video.amazon\resources\cache >nul 2>&1
 
 for /f %%f in ('dir %~dp0 /b /a:d') do if exist %~dp0%%f\addon.xml (
     for /f %%g in ('dir %~dp0%%f /b /a:d /s') do rd /s /q %%g\__pycache__ >nul 2>&1
@@ -31,25 +36,56 @@ for /f %%f in ('dir %~dp0 /b /a:d') do if exist %~dp0%%f\addon.xml (
             )
             if "!line:~-1!"==">" set add=
         )
-        findstr /v /c:"<?xml" %~dp0%%f\addon.xml >> %arc_dir%\addons.xml
-        if not exist %arc_dir%\%%f\%%f-!version!.zip (
-            if exist %arc_dir%\%%f rd /q /s %arc_dir%\%%f >nul 2>&1
-            md %arc_dir%\%%f
-            echo Erstelle %%f v!version!
-            copy %~dp0%%f\addon.xml %arc_dir%\%%f >nul 2>&1
-            if exist %~dp0%%f\icon.png copy %~dp0%%f\icon.png %arc_dir%\%%f >nul 2>&1
-            if exist %~dp0%%f\fanart.jpg copy %~dp0%%f\fanart.jpg %arc_dir%\%%f >nul 2>&1
-            if exist %~dp0%%f\changelog.txt copy %~dp0%%f\changelog.txt %arc_dir%\%%f\changelog-!version!.txt >nul 2>&1
-            %tools_dir%\7za a %arc_dir%\%%f\%%f-!version!.zip %~dp0%%f -tzip > nul
-            %tools_dir%\md5 -l -n %arc_dir%\%%f\%%f-!version!.zip > %arc_dir%\%%f\%%f-!version!.zip.md5
-        ) else (
-            echo %%f v!version! bereits vorhanden
+        if "%%f" neq "%repo_py3%" (
+            findstr /v /c:"<?xml" %~dp0%%f\addon.xml >> %arc_dir_py2%\addons.xml
+            if not exist %arc_dir_py2%\%%f\%%f-!version!.zip (
+                if exist %arc_dir_py2%\%%f rd /q /s %arc_dir_py2%\%%f >nul 2>&1
+                md %arc_dir_py2%\%%f
+                echo Erstelle %%f v!version!
+                copy %~dp0%%f\addon.xml %arc_dir_py2%\%%f >nul 2>&1
+                if exist %~dp0%%f\icon.png copy %~dp0%%f\icon.png %arc_dir_py2%\%%f >nul 2>&1
+                if exist %~dp0%%f\fanart.jpg copy %~dp0%%f\fanart.jpg %arc_dir_py2%\%%f >nul 2>&1
+                if exist %~dp0%%f\changelog.txt copy %~dp0%%f\changelog.txt %arc_dir_py2%\%%f\changelog-!version!.txt >nul 2>&1
+                %tools_dir%\7za a %arc_dir_py2%\%%f\%%f-!version!.zip %~dp0%%f -tzip > nul
+                %tools_dir%\md5 -l -n %arc_dir_py2%\%%f\%%f-!version!.zip > %arc_dir_py2%\%%f\%%f-!version!.zip.md5
+            ) else (
+                echo %%f v!version! bereits vorhanden
+            )
         )
+        if exist %xml_dir_py3%\%%f.xml (
+            if not exist %arc_dir_py3%\%%f\%%f-!version!.zip (
+                if exist %arc_dir_py3%\%%f rd /q /s %arc_dir_py3%\%%f >nul 2>&1
+                md %arc_dir_py3%\%%f
+                copy %arc_dir_py2%\%%f\*.* %arc_dir_py3%\%%f\ >nul 2>&1
+                call :find_repl !version! "%xml_dir_py3%\%%f.xml" > %arc_dir_py3%\%%f\addon.xml
+                cmd /c "cd /d %arc_dir_py3% ^&^& %tools_dir%\7za a %arc_dir_py3%\%%f\%%f-!version!.zip %%f\addon.xml -tzip" > nul
+                %tools_dir%\md5 -l -n %arc_dir_py3%\%%f\%%f-!version!.zip > %arc_dir_py3%\%%f\%%f-!version!.zip.md5
+            )
+            findstr /v /c:"<?xml" %arc_dir_py3%\%%f\addon.xml >> %arc_dir_py3%\addons.xml
+            del /q "%temp%\addon.xml" >nul 2>&1
+        )   
     )
 )
 
 if not "%1"=="clean" (
-    echo ^</addons^>>> %arc_dir%\addons.xml
-    %tools_dir%\md5 -l -n %arc_dir%\addons.xml > %arc_dir%\addons.xml.md5
+    echo ^</addons^>>> %arc_dir_py2%\addons.xml
+    echo ^</addons^>>> %arc_dir_py3%\addons.xml
+    %tools_dir%\md5 -l -n %arc_dir_py2%\addons.xml > %arc_dir_py2%\addons.xml.md5
+    %tools_dir%\md5 -l -n %arc_dir_py3%\addons.xml > %arc_dir_py3%\addons.xml.md5
 )
 pause
+goto :eof
+
+:find_repl
+:::https://www.dostips.com/DtCodeBatchFiles.php#Batch.FindAndReplace
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL DISABLEDELAYEDEXPANSION
+for /f "tokens=1,* delims=]" %%A in ('"type %2|find /n /v """') do (
+    set "line=%%B"
+    if defined line (
+        call set "line=echo.%%line:{ver_add}=%~1%%"
+        call set "line=%%line:{ver_py}=%v_py3%%%"
+        for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X
+    ) ELSE echo.
+)
+goto :eof

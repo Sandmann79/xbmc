f3fbdc6022787d00b19ef42a60b76d99d0cadf5c
Sandmann79 <sandmann79@gmx.net>
2020-03-28 15:24:55 +0100

    add matrix version codes for easier upgrading

--- createxml.bat
+++ createxml.bat
@@ -6,6 +6,7 @@ set arc_dir_py3=%~dp0packages-py3
 set xml_dir_py3=%~dp0xml-py3
 set repo_py3=repository.sandmann79-py3.plugins
 set v_py3=3.0.0
+set add_ver_py3=+matrix.1
 
 
 if not "%1"=="clean" (
@@ -24,18 +25,7 @@ for /f %%f in ('dir %~dp0 /b /a:d') do if exist %~dp0%%f\addon.xml (
     del /q /s %~dp0%%f\*.pyc >nul 2>&1
     if exist %~dp0%%f\.idea rd /q /s %~dp0%%f\.idea >nul 2>&1
     if not "%1"=="clean" (
-        set add=
-        for /f "delims=" %%a in (%~dp0%%f\addon.xml) do (
-            set line=%%a
-            if "!line:~0,6!"=="<addon" set add=1
-            if not "!line!"=="!line:version=!" if "!add!"=="1" (
-                set new=!line:version=ß!
-                for /f "delims=ß tokens=2" %%n in ("!new!") do set new=%%n
-                for /f "delims=^= " %%n in ("!new!") do set new=%%n
-                set version=!new:^"=!
-            )
-            if "!line:~-1!"==">" set add=
-        )
+        call :get_version %~dp0%%f\addon.xml
         set arc_dir=%arc_dir_py2%
         if "%%f"=="%repo_py3%" (set arc_dir=%arc_dir_py3%)
         findstr /v /c:"<?xml" %~dp0%%f\addon.xml >> !arc_dir!\addons.xml
@@ -53,13 +43,16 @@ for /f %%f in ('dir %~dp0 /b /a:d') do if exist %~dp0%%f\addon.xml (
             echo %%f v!version! bereits vorhanden
         )
         if exist %xml_dir_py3%\%%f.xml (
-            if not exist %arc_dir_py3%\%%f\%%f-!version!.zip (
+            set version_py3=!version!%add_ver_py3%
+            if not exist %arc_dir_py3%\%%f\%%f-!version_py3!.zip (
                 if exist %arc_dir_py3%\%%f rd /q /s %arc_dir_py3%\%%f >nul 2>&1
                 md %arc_dir_py3%\%%f
                 copy %arc_dir_py2%\%%f\*.* %arc_dir_py3%\%%f\ >nul 2>&1
-                call :find_repl !version! "%xml_dir_py3%\%%f.xml" > %arc_dir_py3%\%%f\addon.xml
-                cmd /c "cd /d %arc_dir_py3% ^&^& %tools_dir%\7za a %arc_dir_py3%\%%f\%%f-!version!.zip %%f\addon.xml -tzip" > nul
-                %tools_dir%\md5 -l -n %arc_dir_py3%\%%f\%%f-!version!.zip > %arc_dir_py3%\%%f\%%f-!version!.zip.md5
+                ren %arc_dir_py3%\%%f\%%f-!version!.zip %%f-!version_py3!.zip
+                del %arc_dir_py3%\%%f\%%f-!version!.zip.md5
+                call :find_repl !version_py3! "%xml_dir_py3%\%%f.xml" > %arc_dir_py3%\%%f\addon.xml
+                cmd /c "cd /d %arc_dir_py3% ^&^& %tools_dir%\7za a %arc_dir_py3%\%%f\%%f-!version_py3!.zip %%f\addon.xml -tzip" > nul
+                %tools_dir%\md5 -l -n %arc_dir_py3%\%%f\%%f-!version_py3!.zip > %arc_dir_py3%\%%f\%%f-!version_py3!.zip.md5
             )
             findstr /v /c:"<?xml" %arc_dir_py3%\%%f\addon.xml >> %arc_dir_py3%\addons.xml
         )   
@@ -86,5 +79,20 @@ for /f "tokens=1,* delims=]" %%A in ('"type %2|find /n /v """') do (
         call set "line=%%line:{ver_py}=%v_py3%%%"
         for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X
     ) ELSE echo.
+)
+goto :eof
+
+:get_version
+set add=
+for /f "delims=" %%a in (%1) do (
+    set line=%%a
+    if "!line:~0,6!"=="<addon" set add=1
+    if not "!line!"=="!line:version=!" if "!add!"=="1" (
+        set new=!line:version=ß!
+        for /f "delims=ß tokens=2" %%n in ("!new!") do set new=%%n
+        for /f "delims=^= " %%n in ("!new!") do set new=%%n
+        set version=!new:^"=!
+        goto :eof
+    )
 )
 goto :eof

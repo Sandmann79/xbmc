025b336e1f2bee9f0ee4d6bd1325de38c44fe02c
altiOnGithub <tom@alti.at>
2023-03-30 10:33:50 +0200

    Update default.py
    translatePath import with try block, so we are still compatible with Kodi 18/Leia and up to Kodi20/Nexus

--- plugin.program.browser.launcher/default.py
+++ plugin.program.browser.launcher/default.py
@@ -7,7 +7,10 @@ import subprocess
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
-import xbmcvfs
+try:
+    from xbmcvfs import translatePath
+except ImportError:
+    from xbmc import translatePath
 try:
     from urllib.parse import unquote_plus, quote_plus
 except ImportError:
@@ -21,12 +24,12 @@ addonPath = addon.getAddonInfo('path')
 translation = addon.getLocalizedString
 useOwnProfile = addon.getSetting("useOwnProfile") == "true"
 useCustomPath = addon.getSetting("useCustomPath") == "true"
-customPath = xbmcvfs.translatePath(addon.getSetting("customPath"))
+customPath = translatePath(addon.getSetting("customPath"))
 winBrowser = int(addon.getSetting("winBrowser"))
 #prio_values = ['LOW', 'BELOWNORMAL', 'NORMAL', 'ABOVENORMAL', 'HIGH', 'REALTIME' ]
 prio_values = [ 0x00000040, 0x00004000, 0x00000020, 0x00008000, 0x00000080, 0x00000100 ]
 priority = prio_values[int(addon.getSetting("priority"))]
-userDataFolder = xbmcvfs.translatePath("special://profile/addon_data/"+addonID)
+userDataFolder = translatePath("special://profile/addon_data/"+addonID)
 profileFolder = os.path.join(userDataFolder, 'profile')
 siteFolder = os.path.join(userDataFolder, 'sites')
 avBrowsers = ['Standard', 'Internet Explorer', 'Kylo', 'Chrome', 'Firefox', 'Opera']

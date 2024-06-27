bb5c9b5b91a7bec0d61046f15f83d4e3ef099088
Sandmann79 <sandmann79@gmx.net>
2024-06-27 20:47:46 +0200

    [web_api] fix small website changes
    Signed-off-by: Sandmann79 <sandmann79@gmx.net>

--- plugin.video.amazon-test/resources/lib/network.py
+++ plugin.video.amazon-test/resources/lib/network.py
@@ -385,7 +385,7 @@ def GrabJSON(url, postData=None):
                         text = chr(char)
             return text
 
-        text = re.sub('&#?\\w+;', fixup, text) #.replace(u'\uC2A0', ' ')
+        text = re.sub('&#?\\w+;', fixup, text)
         try:
             text = text.encode('latin-1').decode('utf-8')
         except (UnicodeEncodeError, UnicodeDecodeError):
@@ -491,6 +491,8 @@ def GrabJSON(url, postData=None):
                         body = body['props']
                         for p in ['atf', 'btf', 'landingPage', 'browse', 'search', 'categories', 'genre']:
                             Merge(m, body.get(p, {}))
+                        for p in ['content']:
+                            Merge(m, {p: body.get(p, {})})
 
                 if _s.json_dump_raw:
                     # Prune useless/sensitive info
